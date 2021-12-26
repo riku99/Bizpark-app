6815e89f5e1dbe779d5014aee8680a5cac640ddc
riku <riku.suny@gmail.com>
2021-12-26 15:27:46 +0900

    add StatusBar

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,17 +1,23 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { BottomTab } from "./Tab";
+import { StatusBar } from "expo-status-bar";
+import { useColorModeValue } from "native-base";
 
 const Stack = createNativeStackNavigator();
 
 export const RootNavigation = React.memo(() => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Tab"
-        component={BottomTab}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
+    <>
+      {/* AppだとうまくcolorModeが動かなかったのでここで定義 */}
+      <StatusBar style={useColorModeValue("dark", "light")} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tab"
+          component={BottomTab}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </>
   );
 });
