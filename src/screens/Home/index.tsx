de3771376e34af8b049946b45f538551e4fbf66d
riku <riku.suny@gmail.com>
2021-12-26 16:33:50 +0900

    add Image

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,16 @@
-import { Box, VStack, Center, Button, useColorMode } from "native-base";
+import {
+  Box,
+  VStack,
+  Center,
+  Button,
+  useColorMode,
+  Text,
+  Flex,
+} from "native-base";
 import { RootNavigationProp } from "types";
 import { useLayoutEffect } from "react";
-import { SafeAreaView, Text } from "react-native";
 import { Bg } from "src/components/Bg";
+import { Image } from "react-native-expo-image-cache";
 
 type Props = RootNavigationProp<"Tab">;
 
@@ -11,6 +19,7 @@ export const HomeScreen = ({ navigation }: Props) => {
     navigation.setOptions({
       headerShadowVisible: false,
       headerTitle: "",
+      headerLeft: () => <Text>Logo</Text>,
     });
   }, [navigation]);
 
@@ -18,6 +27,19 @@ export const HomeScreen = ({ navigation }: Props) => {
 
   return (
     <Bg flex={1}>
+      <VStack px={4}>
+        <Box bg="blueGray.600" borderRadius="3xl" py={14} px={4}>
+          <Flex direction="row" alignItems="center">
+            <Image
+              uri={uri}
+              style={{ height: 34, width: 34, borderRadius: 34 }}
+            />
+            <Text fontWeight="bold" ml={2}>
+              Riku
+            </Text>
+          </Flex>
+        </Box>
+      </VStack>
       <Button
         position="absolute"
         w={50}
@@ -30,6 +52,9 @@ export const HomeScreen = ({ navigation }: Props) => {
     </Bg>
   );
 };
+
+const uri =
+  "https://scontent-nrt1-1.cdninstagram.com/v/t51.2885-15/e35/269868747_906015456777166_2060572380965546765_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=-6oIGf2294IAX8VBWvP&tn=IvOg5e0MTmVxXJmw&edm=AIQHJ4wBAAAA&ccb=7-4&ig_cache_key=MjczNjQ0NTkxNDc5OTM4NDE0Nw%3D%3D.2-ccb7-4&oh=00_AT8dSS2oIRcdbf3nJoIxbbdHJDlWhJLYJXrWLnc_LAuCqA&oe=61CFCB43&_nc_sid=7b02f1";
 
 const data = [
   {
