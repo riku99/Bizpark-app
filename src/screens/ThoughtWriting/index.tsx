acc665c9bcadf18577053390ef7090adb4b67dd2
riku <riku.suny@gmail.com>
2022-01-03 13:53:34 +0900

    add AccsessoryView

--- src/screens/ThoughtWriting/index.tsx
+++ src/screens/ThoughtWriting/index.tsx
@@ -2,6 +2,7 @@ import React, { useLayoutEffect } from "react";
 import { Box, Pressable, Text, Input, useColorModeValue } from "native-base";
 import { RootNavigationScreenProp } from "src/types";
 import { CloseButton } from "src/components/BackButon";
+import { InputAccessoryView } from "react-native";
 
 type Props = RootNavigationScreenProp<"ThoughtWriting">;
 
@@ -24,28 +25,36 @@ export const ThoughtWritingScreen = ({ navigation }: Props) => {
     });
   }, []);
 
+  const textInputId = "textInput";
+
   return (
     <Box flex={1} px={4}>
-      {/* <Text fontSize={18} fontWeight="bold">
-        タイトル (任意)
-      </Text> */}
       <Input
         borderWidth={0}
         fontSize={17}
         fontWeight="bold"
         placeholder="タイトル (任意)"
         mt={2}
-      />
-
-      <Input
-        borderWidth={0}
-        mt={6}
-        placeholder="テキスト"
-        fontSize={16}
-        multiline
-        h="28%"
         keyboardAppearance={useColorModeValue("light", "dark")}
       />
+
+      <Box h="100%">
+        <Input
+          borderWidth={0}
+          mt={4}
+          placeholder="テキスト"
+          fontSize={16}
+          multiline
+          h="28%"
+          keyboardAppearance={useColorModeValue("light", "dark")}
+          autoFocus
+          inputAccessoryViewID={textInputId}
+        />
+      </Box>
+
+      <InputAccessoryView nativeID={textInputId}>
+        <Box w={40} h={50} bg="blue.900"></Box>
+      </InputAccessoryView>
     </Box>
   );
 };
