3bf6abd94c963a8cb107b85708a22d0223a40c31
riku <riku.suny@gmail.com>
2022-01-09 17:09:11 +0900

    created NewsWebView

--- src/screens/NewsWebView/index.tsx
+++ src/screens/NewsWebView/index.tsx
@@ -1,6 +1,35 @@
-import React from "react";
-import { Box } from "native-base";
+import React, { useLayoutEffect } from "react";
+import { RootNavigationScreenProp } from "src/types";
+import { WebView } from "react-native-webview";
+import { useNewsCacheFragment } from "src/hooks/apollo";
+import { SafeAreaView } from "react-native";
 
-export const NewsWebViewScreen = () => {
-  return <Box></Box>;
+type Props = RootNavigationScreenProp<"NewsWebView">;
+
+export const NewsWebViewScreen = ({ navigation, route }: Props) => {
+  const { id } = route.params;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: "",
+    });
+  }, []);
+
+  const { readNewsFragment } = useNewsCacheFragment();
+  const data = readNewsFragment({ id });
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        source={{ uri: data.link }}
+        style={{ width: "100%", height: "100%" }}
+        allowsInlineMediaPlayback={true}
+        allowsFullscreenVideo={false}
+        mediaPlaybackRequiresUserAction={true}
+      />
+    </SafeAreaView>
+  );
 };
