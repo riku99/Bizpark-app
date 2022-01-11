d947f6407887677dcc4cfec5c79dcf75947c45fb
riku <riku.suny@gmail.com>
2022-01-11 13:09:23 +0900

    update UserItemEdit

--- src/screens/UserItemEdit/index.tsx
+++ src/screens/UserItemEdit/index.tsx
@@ -1,5 +1,5 @@
-import React, { useLayoutEffect } from "react";
-import { Box } from "native-base";
+import React, { useLayoutEffect, useState } from "react";
+import { Box, Input, Text, useColorModeValue, Pressable } from "native-base";
 import { RootNavigationScreenProp } from "src/types";
 
 type Props = RootNavigationScreenProp<"UserItemEdit">;
@@ -16,11 +16,44 @@ export const UserItemEditScreen = ({ navigation, route }: Props) => {
       break;
   }
 
+  const [newValue, setNewValue] = useState(value);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title,
+      headerRight: () => (
+        <Pressable
+          onPress={() => {
+            setValue(newValue);
+            navigation.goBack();
+          }}
+        >
+          <Text fontWeight="bold" color="pink" fontSize="17">
+            保存
+          </Text>
+        </Pressable>
+      ),
     });
-  }, [navigation]);
+  }, [navigation, setValue, newValue]);
 
-  return <Box></Box>;
+  return (
+    <Box flex={1}>
+      <Box h="48" pt="4" px="2">
+        <Input
+          borderWidth="0"
+          borderBottomWidth="1"
+          mt="2"
+          fontSize="16"
+          _focus={{
+            borderBottomColor: "lightGray",
+          }}
+          multiline={type === "bio"}
+          autoFocus
+          keyboardAppearance={useColorModeValue("light", "dark")}
+          onChangeText={setNewValue}
+          defaultValue={value}
+        />
+      </Box>
+    </Box>
+  );
 };
