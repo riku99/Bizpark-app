2e9f15e910279a4352601d5c227944a3e7939756
riku <riku.suny@gmail.com>
2022-01-18 16:53:46 +0900

    fix BaseChat

--- src/components/BaseChat/index.tsx
+++ src/components/BaseChat/index.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, useCallback } from "react";
 import {
   GiftedChat,
   IMessage,
@@ -23,66 +23,88 @@ export const BaseChat = React.memo(({ ...props }: Props) => {
   const inputContainerBg = useColorModeValue("#f0f0f0", "#292522");
   const keyboard = useColorModeValue("light", "dark");
 
+  const renderBubble = useCallback(
+    (props) => {
+      return (
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            left: { backgroundColor: bubbleLeft, paddingVertical: 4 },
+            right: { backgroundColor: bubbleRight, paddingVertical: 4 },
+          }}
+        />
+      );
+    },
+    [bubbleLeft, bubbleRight]
+  );
+
+  const renderMessageText = useCallback(
+    (props) => {
+      return (
+        <MessageText
+          {...props}
+          textStyle={{
+            left: {
+              color: leftTextColor,
+            },
+            right: { color: "white" },
+          }}
+        />
+      );
+    },
+    [leftTextColor]
+  );
+
+  const renderInputToolbar = useCallback(
+    (props) => (
+      <InputToolbar
+        {...props}
+        containerStyle={{
+          backgroundColor: inputContainerBg,
+          borderTopWidth: 0,
+          borderRadius: 20,
+          width: "90%",
+          marginLeft: 20,
+        }}
+      />
+    ),
+    [inputContainerBg]
+  );
+
+  const renderComposer = useCallback(
+    (props) => {
+      return (
+        <Composer
+          {...props}
+          keyboardAppearance={keyboard}
+          textInputStyle={{
+            color: inputTextColor,
+          }}
+        />
+      );
+    },
+    [inputTextColor]
+  );
+
+  const renderSend = useCallback((props) => {
+    return (
+      <Send {...props} containerStyle={styles.sendContainer}>
+        <Text style={styles.sendButtonTitile}>送信</Text>
+      </Send>
+    );
+  }, []);
+
   return (
     <GiftedChat
       alignTop
       placeholder="メッセージを入力"
       bottomOffset={bottom}
       renderTime={() => null}
-      renderBubble={(props) => {
-        return (
-          <Bubble
-            {...props}
-            wrapperStyle={{
-              left: { backgroundColor: bubbleLeft, paddingVertical: 4 },
-              right: { backgroundColor: bubbleRight, paddingVertical: 4 },
-            }}
-          />
-        );
-      }}
-      renderMessageText={(props) => {
-        return (
-          <MessageText
-            {...props}
-            textStyle={{
-              left: {
-                color: leftTextColor,
-              },
-              right: { color: "white" },
-            }}
-          />
-        );
-      }}
-      renderInputToolbar={(props) => (
-        <InputToolbar
-          {...props}
-          containerStyle={{
-            backgroundColor: inputContainerBg,
-            borderTopWidth: 0,
-            borderRadius: 20,
-            width: "90%",
-            marginLeft: 20,
-          }}
-        />
-      )}
-      renderComposer={(props) => {
-        return (
-          <Composer
-            {...props}
-            keyboardAppearance={keyboard}
-            textInputStyle={{
-              color: inputTextColor,
-            }}
-          />
-        );
-      }}
-      renderSend={(props) => {
-        return (
-          <Send {...props} containerStyle={styles.sendContainer}>
-            <Text style={styles.sendButtonTitile}>送信</Text>
-          </Send>
-        );
-      }}
+      renderBubble={renderBubble}
+      renderMessageText={renderMessageText}
+      renderInputToolbar={renderInputToolbar}
+      renderComposer={renderComposer}
+      renderSend={renderSend}
       {...props}
     />
   );
