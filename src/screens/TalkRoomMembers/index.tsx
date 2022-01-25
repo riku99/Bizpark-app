999f00bd0008155104e23fc51fe4c5d6642c4453
riku <riku.suny@gmail.com>
2022-01-25 18:18:18 +0900

    add query

--- src/screens/TalkRoomMembers/index.tsx
+++ src/screens/TalkRoomMembers/index.tsx
@@ -1,15 +1,31 @@
 import React, { useLayoutEffect } from "react";
 import { Box } from "native-base";
 import { RootNavigationScreenProp } from "src/types";
+import { useGetThoughtTalkRoomMembersQuery } from "src/generated/graphql";
+import { Indicator } from "src/components/Indicator";
 
 type Props = RootNavigationScreenProp<"TalkRoomMembers">;
 
-export const TalkRoomMembersScreen = ({ navigation }: Props) => {
+export const TalkRoomMembersScreen = ({ navigation, route }: Props) => {
+  const { talkRoomId } = route.params;
   useLayoutEffect(() => {
     navigation.setOptions({
       title: "メンバー",
     });
   }, [navigation]);
+
+  const { data, fetchMore } = useGetThoughtTalkRoomMembersQuery({
+    variables: {
+      talkRoomId,
+    },
+    fetchPolicy: "network-only",
+  });
+
+  if (!data) {
+    return <Indicator style={{ marginTop: 10 }} />;
+  }
+
+  console.log(data.thoughtTalkRoom.members);
 
   return <Box></Box>;
 };
