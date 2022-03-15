d27556f073484eadf17deb175ed454b7d42ca0fc
riku <riku.suny@gmail.com>
2022-03-15 18:31:18 +0900

    fix d.ts

--- src/react-native-gifted-chat.d.ts
+++ src/react-native-gifted-chat.d.ts
@@ -1,12 +1,5 @@
 import { IMessage } from 'react-native-gifted-chat';
 
-declare module '*.svg' {
-  import React from 'react';
-  import { SvgProps } from 'react-native-svg';
-  const content: React.FC<SvgProps>;
-  export default content;
-}
-
 declare module 'react-native-gifted-chat' {
   interface IMessage {
     replyMessage?: {
