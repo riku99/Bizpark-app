7fd936a1c4811e984b24d7ed87e37914fe9c6076
riku <riku.suny@gmail.com>
2022-03-23 13:48:44 +0900

    fix Notification

--- src/screens/Notifications/index.tsx
+++ src/screens/Notifications/index.tsx
@@ -1,4 +1,9 @@
-import React, { useLayoutEffect, useCallback, useEffect } from 'react';
+import React, {
+  useLayoutEffect,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { Box, HStack, Text, useColorModeValue, Pressable } from 'native-base';
 import { RootNavigationScreenProp } from 'src/types';
 import { UserImage } from 'src/components/UserImage';
@@ -18,6 +23,7 @@ import { Indicator } from 'src/components/Indicator';
 import { StyleSheet } from 'react-native';
 import { InfiniteFlatList } from 'src/components/InfiniteFlatList';
 import { btoa } from 'react-native-quick-base64';
+import { RefreshControl } from 'src/components/RefreshControl';
 
 type Props = RootNavigationScreenProp<'Notifications'>;
 
@@ -37,7 +43,13 @@ export const NotificationsScreen = ({ navigation }: Props) => {
   const textGray = useColorModeValue('lt.textGray', 'dt.textGray');
   const pressed = useColorModeValue('lt.pressed', 'dt.pressed');
 
-  const { data: notificationData, fetchMore } = useGetNotificationsQuery();
+  const {
+    data: notificationData,
+    fetchMore,
+    refetch,
+  } = useGetNotificationsQuery();
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
 
   const { findThoughtTalkRoom } = useFindThoughtTalkRoom();
   const { findNewsTalkRoom } = useFindNewsTalkRoom();
@@ -57,130 +69,140 @@ export const NotificationsScreen = ({ navigation }: Props) => {
         }
       }
     })();
-  }, [notificationData]);
+  }, [notificationData, seeNotificationMutation]);
 
-  const renderItem = useCallback(({ item }: { item: Item }) => {
-    const {
-      performer,
-      createdAt,
-      type: _type,
-      talkRoomType,
-      talkRoomId,
-      thought,
-    } = item.node;
+  const renderItem = useCallback(
+    ({ item }: { item: Item }) => {
+      const {
+        performer,
+        createdAt,
+        type: _type,
+        talkRoomType,
+        talkRoomId,
+        thought,
+      } = item.node;
 
-    const diff = formatDistanceToNow(new Date(Number(createdAt)), {
-      locale: ja,
-      addSuffix: true,
-    });
+      const diff = formatDistanceToNow(new Date(Number(createdAt)), {
+        locale: ja,
+        addSuffix: true,
+      });
 
-    let type: string = '';
-    let message: string = '';
-    switch (_type) {
-      case NotificationType.Reply:
-        type = '返信';
-        message = replyMessage(performer.name);
-        break;
-      case NotificationType.Like:
-        type = 'いいね';
-        message = likeMessage(performer.name);
-        break;
-      case NotificationType.Follow:
-        type = 'フォロー';
-        message = followMessage(performer.name);
-        break;
-    }
-
-    const onPress = () => {
-      if (_type === NotificationType.Reply) {
-        if (!talkRoomType || !talkRoomId) {
-          return;
-        }
-
-        if (talkRoomType === TalkRoomType.Thought) {
-          const room = findThoughtTalkRoom({ id: talkRoomId });
-          if (room) {
-            navigation.navigate('ThoughtTalkRoom', {
-              screen: 'ThoughtTalkRoomMain',
-              params: {
-                id: room.id,
-              },
-            });
-          }
-          return;
-        }
-
-        if (talkRoomType === TalkRoomType.News) {
-          const room = findNewsTalkRoom({ id: talkRoomId });
-          if (room) {
-            navigation.navigate('NewsTalkRoom', {
-              screen: 'NewsTalkRoomMain',
-              params: {
-                id: room.id,
-              },
-            });
-          }
-          return;
-        }
-
-        if (talkRoomType === TalkRoomType.Oneonone) {
-          const room = findOneOnOneTalkRoom({ talkRoomId });
-          if (room) {
-            navigation.navigate('OneOnOneTalkRoom', {
-              screen: 'OneOnOneTalkRoomMain',
-              params: {
-                id: room.id,
-              },
-            });
-          }
-          return;
-        }
+      let type: string = '';
+      let message: string = '';
+      switch (_type) {
+        case NotificationType.Reply:
+          type = '返信';
+          message = replyMessage(performer.name);
+          break;
+        case NotificationType.Like:
+          type = 'いいね';
+          message = likeMessage(performer.name);
+          break;
+        case NotificationType.Follow:
+          type = 'フォロー';
+          message = followMessage(performer.name);
+          break;
       }
 
-      if (_type === NotificationType.Like) {
-        if (thought) {
-          navigation.navigate('Thought', {
-            id: thought.id,
+      const onPress = () => {
+        if (_type === NotificationType.Reply) {
+          if (!talkRoomType || !talkRoomId) {
+            return;
+          }
+
+          if (talkRoomType === TalkRoomType.Thought) {
+            const room = findThoughtTalkRoom({ id: talkRoomId });
+            if (room) {
+              navigation.navigate('ThoughtTalkRoom', {
+                screen: 'ThoughtTalkRoomMain',
+                params: {
+                  id: room.id,
+                },
+              });
+            }
+            return;
+          }
+
+          if (talkRoomType === TalkRoomType.News) {
+            const room = findNewsTalkRoom({ id: talkRoomId });
+            if (room) {
+              navigation.navigate('NewsTalkRoom', {
+                screen: 'NewsTalkRoomMain',
+                params: {
+                  id: room.id,
+                },
+              });
+            }
+            return;
+          }
+
+          if (talkRoomType === TalkRoomType.Oneonone) {
+            const room = findOneOnOneTalkRoom({ talkRoomId });
+            if (room) {
+              navigation.navigate('OneOnOneTalkRoom', {
+                screen: 'OneOnOneTalkRoomMain',
+                params: {
+                  id: room.id,
+                },
+              });
+            }
+            return;
+          }
+        }
+
+        if (_type === NotificationType.Like) {
+          if (thought) {
+            navigation.navigate('Thought', {
+              id: thought.id,
+            });
+          }
+          return;
+        }
+
+        if (_type === NotificationType.Follow) {
+          navigation.navigate('UserProfile', {
+            id: performer.id,
           });
+          return;
         }
-        return;
-      }
+      };
 
-      if (_type === NotificationType.Follow) {
+      const onAvatarPress = () => {
         navigation.navigate('UserProfile', {
           id: performer.id,
         });
-        return;
-      }
-    };
+      };
 
-    const onAvatarPress = () => {
-      navigation.navigate('UserProfile', {
-        id: performer.id,
-      });
-    };
-
-    return (
-      <Pressable
-        px="4"
-        py="4"
-        _pressed={{
-          bg: pressed,
-        }}
-        onPress={onPress}
-      >
-        <HStack>
-          <Pressable onPress={onAvatarPress}>
-            <UserImage size="10" uri={performer.imageUrl} />
-          </Pressable>
-          <Box ml="4" flexShrink="1">
-            <Text fontWeight="bold">{message}</Text>
-            <Text color={textGray}>{`${diff} ・ ${type}`}</Text>
-          </Box>
-        </HStack>
-      </Pressable>
-    );
-  }, []);
+      return (
+        <Pressable
+          px="4"
+          py="4"
+          _pressed={{
+            bg: pressed,
+          }}
+          onPress={onPress}
+        >
+          <HStack>
+            <Pressable onPress={onAvatarPress}>
+              <UserImage size="10" uri={performer.imageUrl} />
+            </Pressable>
+            <Box ml="4" flexShrink="1">
+              <Text fontWeight="bold">{message}</Text>
+              <Text color={textGray}>{`${diff} ・ ${type}`}</Text>
+            </Box>
+          </HStack>
+        </Pressable>
+      );
+    },
+    [
+      findNewsTalkRoom,
+      findOneOnOneTalkRoom,
+      findThoughtTalkRoom,
+      navigation,
+      pressed,
+      textGray,
+    ]
+  );
 
   const infiniteLoad = async () => {
     if (notificationData?.notifications.pageInfo.hasNextPage) {
@@ -190,6 +212,17 @@ export const NotificationsScreen = ({ navigation }: Props) => {
           after: cursor ? btoa(cursor) : undefined,
         },
       });
+    }
+  };
+
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+    try {
+      await refetch();
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsRefreshing(false);
     }
   };
 
@@ -203,6 +236,9 @@ export const NotificationsScreen = ({ navigation }: Props) => {
       renderItem={renderItem}
       keyExtractor={(item) => item.node.id.toString()}
       infiniteLoad={infiniteLoad}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+      }
     />
   );
 };
