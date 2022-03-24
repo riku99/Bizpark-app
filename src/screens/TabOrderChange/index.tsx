29409d10826553b241d85fd33099d6fdcf86ced5
riku <riku.suny@gmail.com>
2022-03-24 19:20:05 +0900

    update TabOrderChange

--- src/screens/TabOrderChange/index.tsx
+++ src/screens/TabOrderChange/index.tsx
@@ -1,6 +1,33 @@
-import React, { useLayoutEffect } from 'react';
-import { Box } from 'native-base';
+import React, { useLayoutEffect, useCallback, useState } from 'react';
+import { Box, Pressable, Text, Divider } from 'native-base';
 import { RootNavigationScreenProp } from 'src/types';
+import DraggableFlatList, {
+  ScaleDecorator,
+  RenderItemParams,
+} from 'react-native-draggable-flatlist';
+
+const initialData = [
+  {
+    id: 1,
+    name: 'ビジネス',
+  },
+  {
+    id: 2,
+    name: '政治',
+  },
+  {
+    id: 3,
+    name: '金融経済',
+  },
+  {
+    id: 4,
+    name: '社会',
+  },
+  {
+    id: 5,
+    name: 'フォロー',
+  },
+];
 
 type Props = RootNavigationScreenProp<'TabOrderChange'>;
 
@@ -11,5 +38,32 @@ export const TabOrderChangeScreen = ({ navigation }: Props) => {
     });
   }, [navigation]);
 
-  return <Box></Box>;
+  const [data, setData] = useState(initialData);
+
+  const renderItem = useCallback(
+    ({ item, drag, isActive }: RenderItemParams<typeof data[number]>) => {
+      return (
+        <>
+          <ScaleDecorator>
+            <Pressable onLongPress={drag} disabled={isActive} py="4" px="6">
+              <Text fontWeight="bold">{item.name}</Text>
+            </Pressable>
+          </ScaleDecorator>
+          <Divider ml="5" />
+        </>
+      );
+    },
+    []
+  );
+
+  return (
+    <Box flex="1">
+      <DraggableFlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        onDragEnd={({ data }) => setData(data)}
+      />
+    </Box>
+  );
 };
