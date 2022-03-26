94c5b3c4f73a61301fae4a5c21a83c947410447a
riku <riku.suny@gmail.com>
2022-03-26 15:26:28 +0900

    add NewsTabOrderChange

--- src/screens/NewsTabOrderChange/index.tsx
+++ src/screens/NewsTabOrderChange/index.tsx
@@ -1,11 +1,12 @@
-import React, { useLayoutEffect, useCallback } from 'react';
-import { Box } from 'native-base';
+import React, { useLayoutEffect, useCallback, useState } from 'react';
+import { Box, Pressable, Text, Divider } from 'native-base';
 import { RootNavigationScreenProp } from 'src/types';
 import { CloseButton } from 'src/components/BackButon';
 import DraggableFlatList, {
   ScaleDecorator,
   RenderItemParams,
 } from 'react-native-draggable-flatlist';
+import { useNewsTabOrder } from 'src/hooks/newsTabOrder';
 
 type Props = RootNavigationScreenProp<'NewsTabOrderChange'>;
 
@@ -17,7 +18,42 @@ export const NewsTabOrderChangeScreen = ({ navigation }: Props) => {
     });
   }, [navigation]);
 
-  const renderItem = useCallback(() => {}, []);
+  const { newsTabOrder, setNewsTabOrder, setChangedNewsTabOrder } =
+    useNewsTabOrder();
 
-  return <Box flex="1"></Box>;
+  const [data, setData] = useState(newsTabOrder);
+
+  const renderItem = useCallback(
+    ({ item, drag, isActive }: RenderItemParams<typeof data[number]>) => {
+      return (
+        <>
+          <ScaleDecorator>
+            <Pressable onLongPress={drag} disabled={isActive} py="4" px="6">
+              <Text fontWeight="bold">{item.label}</Text>
+            </Pressable>
+          </ScaleDecorator>
+          <Divider ml="5" />
+        </>
+      );
+    },
+    []
+  );
+
+  return (
+    <Box flex="1">
+      <Text alignSelf="center" mt="4">
+        長押しで並べ替え
+      </Text>
+      <DraggableFlatList
+        data={data}
+        keyExtractor={(item) => item.key}
+        renderItem={renderItem}
+        onDragEnd={({ data: _data }) => {
+          setData(_data);
+          setNewsTabOrder(_data);
+          setChangedNewsTabOrder(true);
+        }}
+      />
+    </Box>
+  );
 };
