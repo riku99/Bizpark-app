ed1238c418035879943cc111a8f1ebf15e04fd5c
riku <riku.suny@gmail.com>
2022-06-18 18:04:19 +0900

    fix joining news talk room

--- src/screens/NewsWebView/JoinTalkButton.tsx
+++ src/screens/NewsWebView/JoinTalkButton.tsx
@@ -1,13 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
 import { Box, Button } from 'native-base';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Alert, StyleSheet } from 'react-native';
 import { useToast } from 'react-native-toast-notifications';
 import { CloseButton } from 'src/components/CloseButton';
 import {
-  CustomErrorResponseCode,
   GetNewsTalkRoomsDocument,
   GetNewsTalkRoomsQuery,
+  NewsTalkRoomJoinError,
   useJoinNewsTalkRoomMutation,
 } from 'src/generated/graphql';
 import { useFindNewsTalkRoomFromNewsId } from 'src/hooks/newsTalkRoom';
@@ -65,14 +65,29 @@ export const JoinTalkButton = ({ onCloseButtonPress, newsId }: Props) => {
             }
           },
         });
-      } catch (e) {
-        console.log(e);
-        const gqlError = getGraphQLError(e, 0);
-        if (
-          gqlError &&
-          gqlError.code === CustomErrorResponseCode.InvalidRequest
-        ) {
-          toast.show(gqlError.message, { type: 'danger' });
+      } catch (errors) {
+        const gqlError = getGraphQLError(errors, 0);
+        if (gqlError) {
+          if (gqlError.code === NewsTalkRoomJoinError.UserRemoved) {
+            Alert.alert('現在このトークルームに参加することができません');
+            return;
+          }
+
+          if (gqlError.code === NewsTalkRoomJoinError.UpperLimit) {
+            Alert.alert(
+              '参加できるトーク数が上限に達しています',
+              '無料プランのユーザーが参加できるトーク数は月に6つまでです。プランをアップグレードすることで無制限に参加できるようになります！',
+              [
+                {
+                  text: 'OK',
+                  onPress: () => {
+                    navigation.navigate('IAP');
+                  },
+                },
+              ]
+            );
+            return;
+          }
         }
       } finally {
         spinnerVisibleVar(false);
